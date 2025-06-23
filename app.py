af1210587e7b19656b53b140d0ac6bb4904b59f8
lanlanjr <allanibojr@cspc.edu.ph>
2025-06-23 08:38:23 +0800

    Add interactive print statements for user information in app.py

--- app.py
+++ app.py
@@ -7,4 +7,14 @@ print("Hello World")
 
 
 
-
+print("===========================lalanjr")
+print("how are you?")
+print("I am fine")
+print("what is your name?")
+print("my name is John")
+print("what is your age?")
+print("I am 20 years old")
+print("what is your gender?")
+print("I am male")
+print("what is your email?")
+print("my email is john@gmail.com")
